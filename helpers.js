ff7cd4fc11a692b80e4bf165c73c49674ce27c27
Levi Butcher <lbutche3@wvup.edu>
2019-01-02 19:06:45 -0500

    Moment JS for dates

--- helpers.js
+++ helpers.js
@@ -1,2 +1,5 @@
 // Dump is a handy debugging function we can use to sort of "console.log" our data
 exports.dump = (obj) => JSON.stringify(obj, null, 2)
+
+// Moment JS for dates in views
+exports.moment = require('moment')
