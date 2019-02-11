86043b1513f1871dbaf6fc1ec512a2d914af84ad
LeviButcher <lbutche3@wvup.edu>
2019-02-11 16:31:04 -0500

    Webpack copy plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,45 @@
-const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const autoprefixer = require('autoprefixer')
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const autoprefixer = require("autoprefixer");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const postcss = {
-  loader: 'postcss-loader',
+  loader: "postcss-loader",
   options: {
-    plugins () { return [autoprefixer({ browsers: 'last 3 versions' })] }
+    plugins() {
+      return [autoprefixer({ browsers: "last 3 versions" })];
+    }
   }
-}
+};
 
 const config = {
-  mode: 'development',
-  entry: './public/javascript/index.js',
-  devtool: 'source-map',
+  mode: "development",
+  entry: "./public/javascript/index.js",
+  devtool: "source-map",
   output: {
-    filename: 'App.bundle.js',
-    path: path.resolve(__dirname, '/public', 'dist'),
-    publicPath: '/public/dist/'
+    filename: "App.bundle.js",
+    path: path.resolve(__dirname, "/public", "dist"),
+    publicPath: "/public/dist/"
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: [ MiniCssExtractPlugin.loader, 'css-loader?sourceMap', postcss, 'sass-loader?sourceMap' ]
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader?sourceMap",
+          postcss,
+          "sass-loader?sourceMap"
+        ]
       }
     ]
   },
   plugins: [
-    new MiniCssExtractPlugin({ filename: 'App.bundle.css' })
+    new MiniCssExtractPlugin({ filename: "App.bundle.css" }),
+    new CopyWebpackPlugin([{ from: "public/images/", to: "images/" }], {
+      debug: true
+    })
   ]
-}
+};
 
-module.exports = config
+module.exports = config;
